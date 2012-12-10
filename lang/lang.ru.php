d10aa09233209c1e4e924dcce2cd406dfbaa0ecb
alexbirukov <lord_maloy@mail.ru>
2012-12-11 01:14:29 +0600

    Update translation

--- lang/lang.ru.php
+++ lang/lang.ru.php
@@ -7,7 +7,7 @@
 			'email' => 'info@alexbirukov.ru',
 			'website' => 'http://alexbirukov.ru'
 		),
-		'release-date' => '2012-07-06'
+		'release-date' => '2012-12-11'
 	);
 
 	/**
@@ -15,101 +15,8 @@
 	 */
 	$dictionary = array(
 
-		'Tracker Activity' => 
-		'Монитор активности',
-
-		'Tracker' => 
-		'Монитор',
-
-		'Excluded System Elements' => 
-		'Исключить элементы системы',
-
-		'Login/Logout' => 
-		'Вход/выход',
-
-		'Password Reset' => 
-		'Сброс пароля',
-
-		'Excluded Sections' => 
-		'Исключить разделы',
-
-		'Excluded Users' => 
-		'Исключить пользователей',
-
-		'Limit' => 
-		'Ограничение',
-
-		'Filter Query String' => 
-		'Фильтр строки запроса',
-
-		'Activity' => 
-		'Активность',
-
-		'Time' => 
-		'Время',
-
-		'No data available.' => 
-		'Нет доступных данных',
-
-		'Clear All' => 
-		'Очистить всё',
-
-		'Clear all Activity' => 
-		'Очистить всё',
-
-		'A front-end user' => 
-		'Пользователь интерфейса',
-
-		'An unidentified user (%s)' => 
-		'Неопределённый пользователь (%s)',
-
-		'%1$s deleted %2$s.' => 
-		'%1$s удалил %2$s.',
-
-		'%1$s updated %2$s.' => 
-		'%1$s обновил %2$s.',
-
-		'%1$s created %2$s.' => 
-		'%1$s создал %2$s.',
-
-		'%1$s enabled %2$s.' => 
-		'%1$s включил %2$s.',
-
-		'%1$s disabled %2$s.' => 
-		'%1$s отключил %2$s.',
-
-		'%1$s logged in %2$s.' => 
-		'%1$s вошёл в систему %2$s.',
-
-		'%1$s attempted to log in %2$s.' => 
-		'%1$s пытался войти в систему %2$s.',
-
-		'%1$s reset %2$s.' => 
-		'%1$s произвёл сброс %2$s.',
-
-		'%1$s attempted to reset %2$s.' => 
-		'%1$s пытался произвести сброс %2$s.',
-
-		'%1$s changed %2$s.' => 
-		'%1$s изменил %2$s.',
-
-		'%1$s requested to reset %2$s.' => 
-		'%1$s запросил сброс %2$s.',
-
-		'%1$s uninstalled %2$s.' => 
-		'%1$s удалён %2$s.',
-
-		'%1$s %2$s %3$s.' => 
-		'%1$s %2$s %3$s.',
-
-		'template' => 
-		'шаблон',
-
-		'author record' => 
-		false,
-
-		'system preferences' => 
-		'настройки системы',
+		' his/her password' => 
+		' его/её пароль',
 
 		' maintenance mode' => 
 		' режим обслуживания',
@@ -117,7 +24,100 @@
 		' to the back end' => 
 		' через бэк-энд',
 
-		' his/her password' => 
-		' его/её пароль',
+		'%1$s %2$s %3$s.' => 
+		'%1$s %2$s %3$s.',
+
+		'%1$s attempted to log in %2$s.' => 
+		'%1$s пытался войти в систему %2$s.',
+
+		'%1$s attempted to reset %2$s.' => 
+		'%1$s пытался произвести сброс %2$s.',
+
+		'%1$s changed %2$s.' => 
+		'%1$s изменил %2$s.',
+
+		'%1$s created %2$s.' => 
+		'%1$s создал %2$s.',
+
+		'%1$s deleted %2$s.' => 
+		'%1$s удалил %2$s.',
+
+		'%1$s disabled %2$s.' => 
+		'%1$s отключил %2$s.',
+
+		'%1$s enabled %2$s.' => 
+		'%1$s включил %2$s.',
+
+		'%1$s logged in %2$s.' => 
+		'%1$s вошёл в систему %2$s.',
+
+		'%1$s requested to reset %2$s.' => 
+		'%1$s запросил сброс %2$s.',
+
+		'%1$s reset %2$s.' => 
+		'%1$s произвёл сброс %2$s.',
+
+		'%1$s uninstalled %2$s.' => 
+		'%1$s удалён %2$s.',
+
+		'%1$s updated %2$s.' => 
+		'%1$s обновил %2$s.',
+
+		'A front-end user' => 
+		'Пользователь интерфейса',
+
+		'Activity' => 
+		'Активность',
+
+		'An unidentified user (%s)' => 
+		'Неопределённый пользователь (%s)',
+
+		'Clear All' => 
+		'Очистить всё',
+
+		'Clear all Activity' => 
+		'Очистить всё',
+
+		'Excluded Sections' => 
+		'Исключить разделы',
+
+		'Excluded System Elements' => 
+		'Исключить элементы системы',
+
+		'Excluded Users' => 
+		'Исключить пользователей',
+
+		'Filter Query String' => 
+		'Фильтр строки запроса',
+
+		'Limit' => 
+		'Ограничение',
+
+		'Login/Logout' => 
+		'Вход/выход',
+
+		'No data available.' => 
+		'Нет доступных данных',
+
+		'Password Reset' => 
+		'Сброс пароля',
+
+		'Time' => 
+		'Время',
+
+		'Tracker' => 
+		'Монитор',
+
+		'Tracker Activity' => 
+		'Монитор активности',
+
+		'system preferences' => 
+		'настройки системы',
+
+		'template' => 
+		'шаблон',
+
+		'author record' => 
+		'запись автора',
 
 	);
